16c362878dd87aa3ffc1fc508cc59c7216fda243
sarkarpriyam <priyamsarkar876@yahoo.com>
2025-08-03 21:42:38 +0530

    feat: add requirements.txt

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google import genai
 from google.genai import types
 from prompts import system_prompt
 from call_function import call_function, available_functions 
+from config import MAX_ITERS
 
 def main():
     load_dotenv()
@@ -31,41 +32,64 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    generate_content(client, messages, verbose)
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+
 
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt)
-    )
+            model='gemini-2.0-flash-001',
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt
+                )
+            )
 
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    
+
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            messages.append(function_call_content)
+
     if not response.function_calls:
         return response.text
+
 
     function_responses = []
     for function_call_part in response.function_calls:
         function_call_result = call_function(function_call_part, verbose)
         if (
-            not function_call_result.parts
-            or not function_call_result.parts[0].function_response
+                not function_call_result.parts
+                or not function_call_result.parts[0].function_response
         ):
             raise Exception("empty function call result")
 
         if verbose:
-             print(f"-> {function_call_result.parts[0].function_response.response}")
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 
         function_responses.append(function_call_result.parts[0])
 
     if not function_responses:
         raise Exception("no function responses generated, exiting.")
 
+    messages.append(types.Content(role="tool", parts=function_responses))
 
 if __name__ == "__main__":
     main()
